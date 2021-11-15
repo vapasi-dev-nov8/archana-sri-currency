21c62026c7bc2887dae8be652d79507c4d3d55af
SrividhyaA <srividhya.kanth@gmail.com>
2021-11-15 13:08:46 +0530

    Adding two Rupees and comparing two Rupee objects

--- src/main/java/com/currency/Rupee.java
+++ src/main/java/com/currency/Rupee.java
@@ -1,4 +1,23 @@
 package com.currency;
 
 public class Rupee {
+
+    private int value;
+
+    public Rupee(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public boolean compareTwoRupees(Rupee rupee){
+        if(this.getValue() == rupee.getValue()) return true;
+        else return false;
+    }
+
+    public Rupee add(Rupee rupee){
+        return (new Rupee( this.getValue() + rupee.getValue() ));
+    }
 }
--- src/test/java/com/currency/RupeeTest.java
+++ src/test/java/com/currency/RupeeTest.java
@@ -1,7 +1,30 @@
 package com.currency;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RupeeTest {
 
+    @Test
+    public void shouldReturnTrueIfTwoRupeeValuesAreEqual(){
+        Rupee rupee1 = new Rupee(10);
+        Rupee rupee2 = new Rupee(10);
+        assertTrue(rupee1.compareTwoRupees(rupee2));
+    }
+
+    @Test
+    public void shouldReturnFalseIfTwoRupeeValuesAreNotEqual() {
+        Rupee rupee1 = new Rupee(20);
+        Rupee rupee2 = new Rupee(5);
+        assertFalse(rupee1.compareTwoRupees(rupee2));
+    }
+
+    @Test
+    public void shouldReturnAdditionOfTwoRupeeValues(){
+        Rupee rupee1 = new Rupee(5);
+        Rupee rupee2 = new Rupee(25);
+        Rupee resultRupee = new Rupee(30);
+        assertEquals(resultRupee.getValue(),rupee1.add(rupee2).getValue());
+    }
 }
